bb2f6f7e39dec581e6c1b5107e15a8caa71fbd74
Anderson Almeida <70342250+ander-son-almeida@users.noreply.github.com>
2023-02-17 20:42:23 -0300

    teste graficos

--- pages/03_📈_IntegratedMF.py
+++ pages/03_📈_IntegratedMF.py
@@ -209,10 +209,14 @@ with container2:
     
     st.header("Mass function")
     
-    col1, col2, col3 = st.columns(3)
-    col1.metric("$Teste_{a}$", value=np.around(alpha_high_mass,decimals=2))
-    col2.metric("Wind", value=2.0)
-    col3.metric("Humidity", value=3.0)
+    col4, col5, col6 = st.columns(3)
+    
+    with col4:
+        col1.metric("$Teste_{a}$", value=np.around(alpha_high_mass,decimals=2))
+    with col5:
+        col2.metric("Wind", value=2.0)
+    with col6:
+        col3.metric("Humidity", value=3.0)
     
     
     st.write("All populations")
