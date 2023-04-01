ae7c1145c3aa8c09a8e81769ca41d775cde68c23
Anderson Almeida <70342250+ander-son-almeida@users.noreply.github.com>
2023-03-31 22:47:31 -0300

    add page evolution ra and dec

--- pages/04_📊_Synthetic Evolution.py
+++ pages/04_📊_Synthetic Evolution.py
@@ -153,7 +153,7 @@ if st.button(" ▶️ Play"):
                                'DEC': mod_cluster_obs['DEC_ICRS'][ind], 'Mass': mod_cluster_obs['Mass'][ind]})
 
         fig_ra_dec = px.scatter(ra_dec, x = 'RA', y = 'DEC', color= 'Mass', 
-                                color_continuous_scale = 'jet_r', size=1/mod_cluster_obs['Mass'])
+                                color_continuous_scale = 'jet_r', size=mod_cluster_obs['Mass'])
         
         fig_ra_dec.update_layout(coloraxis_colorbar=dict(title="M☉"),
                           yaxis_range=[-65.0,-64.7],
