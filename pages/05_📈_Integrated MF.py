91cbe13eb608ae49692b9742ed7dadf79eb1df52
Anderson Almeida <70342250+ander-son-almeida@users.noreply.github.com>
2023-06-02 21:54:22 -0300

    test filter parameters

--- pages/05_📈_Integrated MF.py
+++ pages/05_📈_Integrated MF.py
@@ -71,7 +71,7 @@ if ticket_on_off:
     # apply filter
     cluster = cluster[filtro]
     cluster = cluster.reset_index()
-    cluster = cluster.to_records()
+    # cluster = cluster.to_records()
     
 
 # Interface: Select clusters name
