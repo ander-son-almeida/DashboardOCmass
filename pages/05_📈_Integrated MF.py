5f36c625001044026ce8cf3277b274397f09e7e3
Anderson Almeida <70342250+ander-son-almeida@users.noreply.github.com>
2023-07-09 00:02:42 -0300

    button download

--- pages/05_📈_Integrated MF.py
+++ pages/05_📈_Integrated MF.py
@@ -116,9 +116,7 @@ with io.BytesIO() as buffer:
         label="Download {}".format(cluster_name),
         data = buffer, # Download buffer
         file_name = '{}.npy'.format(cluster_name),
-        help ='Download the .npy file of the open cluster. In it, you will \n'
-            'find the values of individual masses determined by us, \n'
-           'along with other Gaia parameters.'
+        help ='Download the .npy file of the open cluster. It includes the values of individual masses determined by us.'
     ) 
 
 # bar with fundamental parameters
